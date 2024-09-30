69cdda7a6bbb13b39352367826f3cba131e6ce88
Dorian Loncle <2331027@etu.cegepjonquiere.ca>
2024-09-30 17:17:02 -0400

    finit

--- trigger/Trigger_LevelUp.sql
+++ trigger/Trigger_LevelUp.sql
@@ -1,4 +1,4 @@
-USE 2024_PROG3_RPGMANAGER;
+USE [2024_PROG3_RPGMANAGER];
 GO
 
 CREATE TRIGGER LevelUp
@@ -12,17 +12,17 @@ IF UPDATE (Experience) BEGIN
 	DECLARE @VieRestante AS DECIMAL
 	DECLARE cUpdate CURSOR FOR 
 	SELECT Experience, ExpRequis, PERSONNAGE.Levels, VieMax, VieRestante 
-	FROM PERSONNAGE 
-	JOIN NIVEAU ON NIVEAU.Levels = PERSONNAGE.Levels;
+		FROM PERSONNAGE 
+		JOIN NIVEAU ON NIVEAU.Levels = PERSONNAGE.Levels;
 	OPEN cUpdate;
 	FETCH cUpdate INTO @Experience, @ExpRequis, @Levels, @VieMax, @VieRestante;
 	--Tant que le status n'a pas d'erreur on fait 
 	While (@@FETCH_STATUS=0) BEGIN
-	-- Si l'expérience est égale ou supérieur à l'experience Requise alors on update toutes les statistiques nécessaire 
+	-- Si l'experience est egale ou superieur a l'experience Requise alors on update toutes les statistiques necessaire 
 		IF (@Experience >= @ExpRequis) BEGIN
 			UPDATE PERSONNAGE SET Experience = 0, Levels = @Levels+1;
-			UPDATE PERSONNAGE SET VieMax = AUGMENTER_VIE, VieRestante = AUGMENTER_VIE;
+			UPDATE PERSONNAGE SET VieMax = (SELECT * FROM AUGMENTER_VIE), VieRestante = (SELECT * FROM AUGMENTER_VIE);
 		END;
-		FETCH cUpdate INTO @Experience, @ExpRequis, @GainVie, @Levels, @VieMax, @VieRestante; --Ligne Suivante
+		FETCH cUpdate INTO @Experience, @ExpRequis, @Levels, @VieMax, @VieRestante; --Ligne Suivante
 	END;
 END;
